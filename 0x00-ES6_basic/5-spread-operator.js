86dc502192fc9be3a5076892f559c1e29d8b6a04
Fiona-Nyadero <fionanyadero@gmail.com>
2024-04-15 19:59:39 +0000

    Task5

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,6 +1,3 @@
 export default function concatArrays(array1, array2, string) {
-  let index = 0;
-  for (let index = 0; ;index++)
-    if (index != ',')
-      return Array.from(array1 + array2 + string);
+  return [...array1, ...array2, ...string];
 }
