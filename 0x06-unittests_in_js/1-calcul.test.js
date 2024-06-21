8f85c2650c84cad994dac2872b0c288722a31476
Fiona-Nyadero <fionanyadero@gmail.com>
2024-06-21 22:05:58 +0300

    Task 1-Combining descriptions

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -9,4 +9,11 @@ describe('calculateNumber', () => {
   it('should return -4 when inputs are 1.4 and 4.5', () => {
     assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
   });
+
+  it('should return 1 when inputs are 1.4 and 1', () => {
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 1), 1);
+  });
+  it('should return Error when inputs are 1.4 and 0', () => {
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), Error);
+  });
 });
