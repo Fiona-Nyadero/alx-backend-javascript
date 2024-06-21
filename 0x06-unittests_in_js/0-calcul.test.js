861f93c3a892cc4b73ffe045522b949abe93a4bd
Fiona-Nyadero <fionanyadero@gmail.com>
2024-06-21 05:48:05 +0300

    Task 0. Basic test with Mocha and Node assertion library

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,37 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', () => {
+        it('should return 4 when inputs are 1 and 3', () => {
+                assert.strictEqual(calculateNumber(1, 3), 4);
+        });
+});
+
+describe('calculateNumber', () => {
+        it('should return 6 when inputs are 1 and 4.7', () => {
+                assert.strictEqual(calculateNumber(1, 4.7), 6);
+        });
+});
+
+describe('calculateNumber', () => {
+        it('should return 2 when inputs are 0.5 and 0.5', () => {
+                assert.strictEqual(calculateNumber(0.5, 0.5), 2);
+        });
+
+});
+
+describe('calculateNumber', () => {
+        it('should return -1 when inputs are -0.7 and -0.5', () => {
+                assert.strictEqual(calculateNumber(-0.7, -0.5), -1);
+        });
+
+        it('should return -2 when inputs are -1 and -1', () => {
+                assert.strictEqual(calculateNumber(-1, -1), -2);
+        });
+});
+
+describe('calculateNumber', () => {
+        it('should return 0 when inputs are 0 and 0', () => {
+                assert.strictEqual(calculateNumber(0, 0), 0);
+        });
+});
