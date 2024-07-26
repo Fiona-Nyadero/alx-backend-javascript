636fe98f667df3a2c0762cb85ac72994efc21a0f
Fiona-Nyadero <fionanyadero@gmail.com>
2024-07-26 18:03:06 +0000

    Task 1 correction

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,23 +1,12 @@
-// Display welcome message
-console.log("Welcome to Holberton School, what is your name?");
+console.log('Welcome to Holberton School, what is your name?');
 
-// Read user input & set to utf8
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
-
-// Handle user input
-process.stdin.on('data', function(data) {
-    // Trim the input
-    const name = data.trim();
-
-    // Display the user's name back to user
-    console.log(`Your name is: ${name}`);
-
-    // End the process after handling input
-    process.exit();
+process.stdin.on('readable', () => {
+  const reader = process.stdin.read();
+  if (reader !== null) {
+    process.stdout.write(`Your name is: ${reader}`);
+  }
 });
 
-// Handle process exit
-process.on('exit', function() {
-    console.log("This important software is now closing");
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
 });
